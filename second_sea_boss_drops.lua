2dbfbc2a4f87aa4cc7b4b57cb8ba03b8ffa48097
ZekeGBlox <157068979+ZekeGBlox@users.noreply.github.com>
2025-09-08 21:27:08 +0100

    Create second_sea_boss_drops.lua

---
+++ second_sea_boss_drops.lua
@@ -0,0 +1,33 @@
+return {
+    Weapons = {
+        ["Diamond"] = { "Acidum Rifle" },
+        ["Jeremy"] = { "Black Spikey Coat" },
+        ["Fajita"] = { "Blue Spikey Coat" },
+        ["Don Swan"] = { "Swan Glasses" },
+        ["Smoke Admiral"] = { "Jitte" },
+        ["Awakened Ice Admiral"] = { "Hidden Key" },
+        ["Tide Keeper"] = { "Water Key" },
+        ["Darkbeard"] = { "Dark Fragment" },
+        ["Cursed Captain"] = { "Hellfire Torch", "Blue Spikey Coat" },
+        ["Order"] = { "Microchip" }
+    },
+    
+    Accessories = {
+        ["Jeremy"] = { "Black Spikey Coat" },
+        ["Fajita"] = { "Blue Spikey Coat" },
+        ["Don Swan"] = { "Swan Glasses" },
+        ["Cursed Captain"] = { "Blue Spikey Coat" }
+    },
+    
+    Materials = {
+        ["Diamond"] = { "Diamond" },
+        ["Darkbeard"] = { "Dark Fragment" },
+        ["Awakened Ice Admiral"] = { "Hidden Key" },
+        ["Tide Keeper"] = { "Water Key" },
+        ["Order"] = { "Microchip" }
+    },
+    
+    NoDrop = {
+        -- Bosses that don't drop items
+    }
+}
