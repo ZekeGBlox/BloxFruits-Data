c59be03148eac5d1b059f19bee477fec747a81cf
ZekeGBlox <157068979+ZekeGBlox@users.noreply.github.com>
2025-09-08 21:27:40 +0100

    Create third_sea_boss_drops.lua

---
+++ third_sea_boss_drops.lua
@@ -0,0 +1,34 @@
+return {
+    Weapons = {
+        ["Stone"] = { "Pilot Helmet" },
+        ["Island Empress"] = { "Serpent Bow" },
+        ["Kilo Admiral"] = { "Lei" },
+        ["Captain Elephant"] = { "Twin Hooks" },
+        ["Beautiful Pirate"] = { "Canvander" },
+        ["Longma"] = { "Tushita", "Yama" },
+        ["Cake Queen"] = { "Buddy Sword" },
+        ["rip_indra True Form"] = { "Dark Dagger" },
+        ["Dough King"] = { "Spikey Trident", "Pale Scarf", "Dough King Title" },
+        ["Soul Reaper"] = { "Soul Guitar", "Hallow Scythe" },
+        ["Cake Prince"] = { "Dough King Title" }
+    },
+    
+    Accessories = {
+        ["Stone"] = { "Pilot Helmet" },
+        ["Kilo Admiral"] = { "Lei" },
+        ["rip_indra True Form"] = { "Valkyrie Helmet" },
+        ["Dough King"] = { "Pale Scarf" },
+        ["Cake Prince"] = { "Pale Scarf" }
+    },
+    
+    Materials = {
+        ["Captain Elephant"] = { "Mystic Droplet" },
+        ["rip_indra True Form"] = { "Tushita", "Yama", "Dark Dagger" },
+        ["Dough King"] = { "Conjured Cocoa", "Red Key", "Dough King Title" },
+        ["Cake Prince"] = { "Conjured Cocoa" }
+    },
+    
+    NoDrop = {
+        -- Bosses that don't drop items
+    }
+}
