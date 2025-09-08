fd6f87ead1cde4f7042aadbb889572725b534761
ZekeGBlox <157068979+ZekeGBlox@users.noreply.github.com>
2025-09-08 20:12:40 +0100

    Create first_sea_boss_drops.lua

---
+++ first_sea_boss_drops.lua
@@ -0,0 +1,30 @@
+return {
+    Weapons = {
+        ["The Saw"] = { "Shark Saw" },
+        ["Chief Warden"] = { "Warden’s Sword" },
+        ["Magma Admiral"] = { "Refined Musket" },
+        ["Fishman Lord"] = { "Trident" },
+        ["Wysper"] = { "Bazooka" },
+        ["Thunder God"] = { "Pole (1st Form)" },
+        ["Saber Expert"] = { "Saber" },
+        ["Greybeard"] = { "Bisento" }
+    },
+
+    Accessories = {
+        ["Vice Admiral"] = { "Coat" },
+        ["Warden"] = { "Coat" },
+        ["Swan"] = { "Pink Coat" },
+        ["Cyborg"] = { "Cool Shades" },
+        ["Yeti"] = { "Yeti Fur" }
+    },
+
+    Materials = {
+        ["Fishman Lord"] = { "Fish Tail" }
+    },
+
+    NoDrop = {
+        "Bobby",
+        "Gorilla King",
+        "Ice Admiral" -- progression boss
+    }
+}
