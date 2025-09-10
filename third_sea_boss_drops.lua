5a7ae287e82e0b4b494e6e8d8fe585f1477ff4aa
ZekeGBlox <157068979+ZekeGBlox@users.noreply.github.com>
2025-09-10 18:40:37 +0100

    Update third_sea_boss_drops.lua

--- third_sea_boss_drops.lua
+++ third_sea_boss_drops.lua
@@ -1,34 +1,47 @@
 return {
-    Weapons = {
-        ["Stone"] = { "Pilot Helmet" },
-        ["Island Empress"] = { "Serpent Bow" },
-        ["Kilo Admiral"] = { "Lei" },
-        ["Captain Elephant"] = { "Twin Hooks" },
-        ["Beautiful Pirate"] = { "Canvander" },
-        ["Longma"] = { "Tushita", "Yama" },
-        ["Cake Queen"] = { "Buddy Sword" },
-        ["rip_indra True Form"] = { "Dark Dagger" },
-        ["Dough King"] = { "Spikey Trident", "Pale Scarf", "Dough King Title" },
-        ["Soul Reaper"] = { "Soul Guitar", "Hallow Scythe" },
-        ["Cake Prince"] = { "Dough King Title" }
-    },
-    
-    Accessories = {
-        ["Stone"] = { "Pilot Helmet" },
-        ["Kilo Admiral"] = { "Lei" },
-        ["rip_indra True Form"] = { "Valkyrie Helmet" },
-        ["Dough King"] = { "Pale Scarf" },
-        ["Cake Prince"] = { "Pale Scarf" }
-    },
-    
-    Materials = {
-        ["Captain Elephant"] = { "Mystic Droplet" },
-        ["rip_indra True Form"] = { "Tushita", "Yama", "Dark Dagger" },
-        ["Dough King"] = { "Conjured Cocoa", "Red Key", "Dough King Title" },
-        ["Cake Prince"] = { "Conjured Cocoa" }
-    },
-    
-    NoDrop = {
-        -- Bosses that don't drop items
-    }
+  Weapons = {
+    ["Island Empress"]     = { "Serpent Bow" },
+    ["Captain Elephant"]   = { "Twin Hooks" },
+    ["Beautiful Pirate"]   = { "Canvander" },
+    ["Cake Queen"]         = { "Buddy Sword" },
+    ["Soul Reaper"]        = { "Hallow Scythe", "Soul Guitar" },
+    ["rip_indra True Form"]= { "Dark Dagger" }
+  },
+
+  Accessories = {
+    ["Stone"]              = { "Pilot Helmet" },
+    ["Kilo Admiral"]       = { "Lei" },
+    ["rip_indra True Form"]= { "Valkyrie Helmet" },
+    ["Dough King"]         = { "Pale Scarf" },
+    ["Cake Prince"]        = { "Pale Scarf" }
+  },
+
+  QuestItems = {
+    ["Longma"]             = { "Tushita", "Yama" },
+    ["Dough King"]         = { "Red Key", "Dough King Title" },
+    ["Cake Prince"]        = { "Dough King Title" }
+  },
+
+  Materials = {
+    ["Captain Elephant"]   = { "Mystic Droplet" },
+    ["Dough King"]         = { "Conjured Cocoa" },
+    ["Cake Prince"]        = { "Conjured Cocoa" }
+  },
+
+  MobsMaterials = {
+    ["Sea Soldier"]        = { "Mystic Droplet" },
+    ["Water Fighter"]      = { "Mystic Droplet" },
+    ["Dragon Crew Warrior"]= { "Dragon Scale" },
+    ["Dragon Crew Archer"] = { "Dragon Scale" },
+    ["Cocoa Warrior"]          = { "Conjured Cocoa" },
+    ["Chocolate Bar Battler"]  = { "Conjured Cocoa" },
+    ["Reborn Skeleton"]    = { "Bones" },
+    ["Living Zombie"]      = { "Bones" },
+    ["Demonic Soul"]       = { "Bones" },
+    ["Possessed Mummy"]    = { "Bones" },
+    ["Haunted Crew Member"]= { "Bones" },
+    ["Ghost Shark"]        = { "Bones" }
+  },
+
+  NoDrop = {}
 }
