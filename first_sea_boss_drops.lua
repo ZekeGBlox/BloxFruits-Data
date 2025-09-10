d56bcb84c251baa1767168251026cbacd4f490ff
ZekeGBlox <157068979+ZekeGBlox@users.noreply.github.com>
2025-09-10 18:39:53 +0100

    Update first_sea_boss_drops.lua

--- first_sea_boss_drops.lua
+++ first_sea_boss_drops.lua
@@ -1,30 +1,56 @@
 return {
-    Weapons = {
-        ["The Saw"] = { "Shark Saw" },
-        ["Chief Warden"] = { "Warden’s Sword" },
-        ["Magma Admiral"] = { "Refined Musket" },
-        ["Fishman Lord"] = { "Trident" },
-        ["Wysper"] = { "Bazooka" },
-        ["Thunder God"] = { "Pole (1st Form)" },
-        ["Saber Expert"] = { "Saber" },
-        ["Greybeard"] = { "Bisento" }
-    },
+  Weapons = {
+    ["The Saw"]        = { "Shark Saw" },
+    ["Chief Warden"]   = { "Wardens Sword" },
+    ["Magma Admiral"]  = { "Refined Musket" },
+    ["Fishman Lord"]   = { "Trident" },
+    ["Wysper"]         = { "Bazooka" },
+    ["Thunder God"]    = { "Pole (1st Form)" },
+    ["Saber Expert"]   = { "Saber" }
+  },
 
-    Accessories = {
-        ["Vice Admiral"] = { "Coat" },
-        ["Warden"] = { "Coat" },
-        ["Swan"] = { "Pink Coat" },
-        ["Cyborg"] = { "Cool Shades" },
-        ["Yeti"] = { "Yeti Fur" }
-    },
+  Accessories = {
+    ["Vice Admiral"]   = { "Coat" },
+    ["Swan"]           = { "Pink Coat" },
+    ["Cyborg"]         = { "Cool Shades" }
+  },
 
-    Materials = {
-        ["Fishman Lord"] = { "Fish Tail" }
-    },
+  QuestItems = {},
 
-    NoDrop = {
-        "Bobby",
-        "Gorilla King",
-        "Ice Admiral" -- progression boss
-    }
+  Materials = {
+    ["Yeti"]           = { "Yeti Fur" }
+  },
+
+  MobsMaterials = {
+    ["Pirate"]            = { "Leather", "Scrap Metal" },
+    ["Brute"]             = { "Leather", "Scrap Metal" },
+    ["Gladiator"]         = { "Leather", "Scrap Metal" },
+    ["Mercenary"]         = { "Leather", "Scrap Metal" },
+    ["Marine Captain"]    = { "Leather", "Scrap Metal" },
+    ["Lab Subordinate"]   = { "Leather", "Scrap Metal" },
+    ["Pirate Millionaire"]= { "Leather", "Scrap Metal" },
+    ["Pistol Billionaire"]= { "Leather", "Scrap Metal" },
+    ["Forest Pirate"]     = { "Leather", "Scrap Metal" },
+    ["Jungle Pirate"]     = { "Leather", "Scrap Metal" },
+    ["God's Guard"]       = { "Angel Wings" },
+    ["Shanda"]            = { "Angel Wings" },
+    ["Royal Squad"]       = { "Angel Wings" },
+    ["Royal Soldier"]     = { "Angel Wings" },
+    ["Wysper"]            = { "Angel Wings" },
+    ["Thunder God"]       = { "Angel Wings" },
+    ["Magma Admiral Guard"] = { "Magma Ore" },
+    ["Magma NPC"]           = { "Magma Ore" },
+    ["Fishman Warrior"]   = { "Fish Tail" },
+    ["Fishman Commando"]  = { "Fish Tail" }
+  },
+
+  NoDrop = {
+    "Bobby",
+    "Gorilla King",
+    "Warden",
+    "Ice Admiral",
+    "Mob Leader",
+    "Greybeard"
+  }
 }
+ 
