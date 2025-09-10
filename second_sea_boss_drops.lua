7b65e0d09fd86403d2ebee51b1eb0bc14e246285
ZekeGBlox <157068979+ZekeGBlox@users.noreply.github.com>
2025-09-10 18:23:52 +0100

    Update second_sea_boss_drops.lua

--- second_sea_boss_drops.lua
+++ second_sea_boss_drops.lua
@@ -1,33 +1,53 @@
 return {
-    Weapons = {
-        ["Diamond"] = { "Acidum Rifle" },
-        ["Jeremy"] = { "Black Spikey Coat" },
-        ["Fajita"] = { "Blue Spikey Coat" },
-        ["Don Swan"] = { "Swan Glasses" },
-        ["Smoke Admiral"] = { "Jitte" },
-        ["Awakened Ice Admiral"] = { "Hidden Key" },
-        ["Tide Keeper"] = { "Water Key" },
-        ["Darkbeard"] = { "Dark Fragment" },
-        ["Cursed Captain"] = { "Hellfire Torch", "Blue Spikey Coat" },
-        ["Order"] = { "Microchip" }
-    },
-    
-    Accessories = {
-        ["Jeremy"] = { "Black Spikey Coat" },
-        ["Fajita"] = { "Blue Spikey Coat" },
-        ["Don Swan"] = { "Swan Glasses" },
-        ["Cursed Captain"] = { "Blue Spikey Coat" }
-    },
-    
-    Materials = {
-        ["Diamond"] = { "Diamond" },
-        ["Darkbeard"] = { "Dark Fragment" },
-        ["Awakened Ice Admiral"] = { "Hidden Key" },
-        ["Tide Keeper"] = { "Water Key" },
-        ["Order"] = { "Microchip" }
-    },
-    
-    NoDrop = {
-        -- Bosses that don't drop items
-    }
+  Weapons = {
+    ["Diamond"] = { "Acidum Rifle" },
+    ["Smoke Admiral"] = { "Jitte" },
+    ["Cursed Captain"] = { "Hellfire Torch" }
+  },
+
+  Accessories = {
+    ["Jeremy"] = { "Black Spikey Coat" },
+    ["Fajita"] = { "Blue Spikey Coat" },
+    ["Don Swan"] = { "Swan Glasses" },
+    ["Cursed Captain"] = { "Blue Spikey Coat" }
+  },
+
+  QuestItems = {
+    ["Awakened Ice Admiral"] = { "Hidden Key" },   -- opens Library/door chain
+    ["Tide Keeper"]         = { "Water Key" },     -- progress to Third Sea chain
+    ["Darkbeard"]           = { "Dark Fragment" }, -- open Darkbeard raid
+    ["Order"]               = { "Microchip" }      -- Law raid chip
+  },
+
+  Materials = {
+    ["Diamond"] = { "Diamond" }
+  },
+
+  -- Mob
+  MobsMaterials = {
+    -- Cursed Ship (Ectoplasm)
+    ["Ship Deckhand"]  = { "Ectoplasm" },
+    ["Ship Steward"]   = { "Ectoplasm" },
+    ["Ship Officer"]   = { "Ectoplasm" },
+    ["Ship Engineer"]  = { "Ectoplasm" },
+
+    -- Magma Ore (Hot/Cold & Magma areas)
+    ["Military Soldier"] = { "Magma Ore" },
+    ["Military Spy"]     = { "Magma Ore" },
+    ["Magma Admiral"]    = { "Magma Ore" },
+    ["Magma Ninja"]      = { "Magma Ore" },
+    ["Lava Pirate"]      = { "Magma Ore" },
+
+    -- Leather / Scrap (generic mats – useful for upgrades)
+    ["Pirate"]            = { "Leather", "Scrap Metal" },
+    ["Brute"]             = { "Leather", "Scrap Metal" },
+    ["Gladiator"]         = { "Leather", "Scrap Metal" },
+    ["Mercenary"]         = { "Leather", "Scrap Metal" },
+    ["Marine Captain"]    = { "Leather", "Scrap Metal" },
+    ["Lab Subordinate"]   = { "Leather", "Scrap Metal" },
+    ["Pirate Millionaire"]= { "Leather", "Scrap Metal" },
+    ["Pistol Billionaire"]= { "Leather", "Scrap Metal" },
+    ["Forest Pirate"]     = { "Leather", "Scrap Metal" },
+    ["Jungle Pirate"]     = { "Leather", "Scrap Metal" }
+  }
 }
