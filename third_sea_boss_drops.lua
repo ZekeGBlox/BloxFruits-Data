55a53eea20200690df09f00c44cb7f5ed06b1083
ZekeGBlox <157068979+ZekeGBlox@users.noreply.github.com>
2026-09-03 16:36:15 +0100

    Update boss drops and materials in third sea

--- third_sea_boss_drops.lua
+++ third_sea_boss_drops.lua
@@ -5,27 +5,43 @@ return {
     ["Beautiful Pirate"]   = { "Canvander" },
     ["Cake Queen"]         = { "Buddy Sword" },
     ["Soul Reaper"]        = { "Hallow Scythe", "Soul Guitar" },
-    ["rip_indra True Form"]= { "Dark Dagger" }
+    ["rip_indra True Form"]= { "Dark Dagger" },
+    ["Dough King"]         = { "Spikey Trident" },
+    ["Cake Prince"]        = { "Spikey Trident" }
   },
 
   Accessories = {
     ["Stone"]              = { "Pilot Helmet" },
     ["Kilo Admiral"]       = { "Lei" },
-    ["rip_indra True Form"]= { "Valkyrie Helmet" },
+    ["rip_indra True Form"]= { "Valkyrie Helm" },
     ["Dough King"]         = { "Pale Scarf" },
-    ["Cake Prince"]        = { "Pale Scarf" }
+    ["Cake Prince"]        = { "Pale Scarf" },
+    ["Soul Reaper"]        = { "Holy Crown" },
+
+    -- Elite Pirates are just same pool of fucking items
+    ["Diablo"]  = { "Hunter Cape (Black)", "Hunter Cape (Red)", "Hunter Cape (Green)", "Bandanna (Black)", "Bandanna (Red)", "Bandanna (Green)" },
+    ["Deandre"] = { "Hunter Cape (Black)", "Hunter Cape (Red)", "Hunter Cape (Green)", "Bandanna (Black)", "Bandanna (Red)", "Bandanna (Green)" },
+    ["Urban"]   = { "Hunter Cape (Black)", "Hunter Cape (Red)", "Hunter Cape (Green)", "Bandanna (Black)", "Bandanna (Red)", "Bandanna (Green)" },
+
+    ["Tyrant of the Skies"] = { "Feathered Visage" }
   },
 
   QuestItems = {
     ["Longma"]             = { "Tushita", "Yama" },
     ["Dough King"]         = { "Red Key", "Dough King Title" },
-    ["Cake Prince"]        = { "Dough King Title" }
+    ["Cake Prince"]        = { "Dough King Title" },
+
+    -- Elite Hunter targets, God's Chalice gates Tushita and Dough King
+    ["Diablo"]             = { "God's Chalice" },
+    ["Deandre"]            = { "God's Chalice" },
+    ["Urban"]              = { "God's Chalice" }
   },
 
   Materials = {
     ["Captain Elephant"]   = { "Mystic Droplet" },
-    ["Dough King"]         = { "Conjured Cocoa" },
-    ["Cake Prince"]        = { "Conjured Cocoa" }
+    ["Dough King"]         = { "Conjured Cocoa", "Mirror Fractal" },
+    ["Cake Prince"]        = { "Conjured Cocoa" },
+    ["Tyrant of the Skies"]= { "Fire Feather", "Moonstone" }
   },
 
   MobsMaterials = {
